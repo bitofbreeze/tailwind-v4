b1b6421fba40379b6109d4692528556eba37c823
f <>
2024-10-02 00:31:22 +0200

    uncomment issue 1

--- app/root.tsx
+++ app/root.tsx
@@ -10,11 +10,11 @@ import type { LinksFunction } from "@remix-run/node";
 // Begin Issue 1: vite url imports break whereas they worked in alpha.15
 // Uncaught SyntaxError: The requested module '/app/root.css?url' does not provide an export named 'default' (at root.tsx:10:8)
 // Switch the commented and uncommented line below to get to Issue 2
-// import cssUrl from "./root.css?url";
-// export const links: LinksFunction = () => [
-//   {rel: 'stylesheet', href: cssUrl}
-// ];
-import './root.css';
+import cssUrl from "./root.css?url";
+export const links: LinksFunction = () => [
+  {rel: 'stylesheet', href: cssUrl}
+];
+// import './root.css';
 // End Issue 1
 
 export function Layout({ children }: { children: React.ReactNode }) {
