aedd626b1ed54a60ae009397251535d245edcac9
f <>
2024-10-02 00:27:46 +0200

    init

--- app/root.tsx
+++ app/root.tsx
@@ -7,20 +7,15 @@ import {
 } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
 
-import "./tailwind.css";
-
-export const links: LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
-];
+// Begin Issue 1: vite url imports break whereas they worked in alpha.15
+// Uncaught SyntaxError: The requested module '/app/root.css?url' does not provide an export named 'default' (at root.tsx:10:8)
+// Switch the commented and uncommented line below to get to Issue 2
+// import cssUrl from "./root.css?url";
+// export const links: LinksFunction = () => [
+//   {rel: 'stylesheet', href: cssUrl}
+// ];
+import './root.css';
+// End Issue 1
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
